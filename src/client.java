82cb6f5a1327c1e3fddc1e14b5057557d0065ea5
Sameera Lanka <slanka@sawyerbrain.ece.ncsu.edu>
2018-04-21 16:34:56 -0400

    add folder

---
+++ src/client.java
@@ -0,0 +1,219 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+
+class segment{
+	int id;
+	String data;
+	segment next;
+
+	public segment(int id, String data){
+		this.id = id;
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class client{
+	private static segment head;
+
+	public client(){
+		head = null;
+	}
+
+	private static int decimal(String substring){
+		//Convert string to decimal integer value
+		int decimalValue = Integer.parseInt(substring, 2);
+		return decimalValue;
+	}
+
+	
+	public static String calcChecksum(byte[] buf) {
+		//Return a 16-bit checksum for a byte array payload (binary string)
+		int length = buf.length;
+	    int i = 0;
+	    int sum = 0;
+	    while (length > 0) {
+	        sum += (buf[i++]&0xff) << 8;
+	        if ((--length)==0) break;
+	        sum += (buf[i++]&0xff);
+	        --length;
+	    }
+
+	    int checksum =  (~((sum & 0xFFFF)+(sum >> 16)))&0xFFFF;
+	    return String.format("%16s", Integer.toBinaryString(checksum)).replace(' ', '0');
+
+	}
+	
+	public static byte[] generateHeader(int seqNumInt, byte[] segData) {
+		//Create segment header containing sequence num, checksum and dataPacket type
+		String seqNum = Integer.toBinaryString(seqNumInt);
+		String checksum = calcChecksum(segData);
+		for (int i = seqNum.length(); i < 32; i++) {
+			seqNum = "0" + seqNum;
+		}
+		String header = seqNum + checksum + "0101010101010101";
+		return header.getBytes();
+	}
+
+	public static int generateSegments(byte[] fileData, int MSS) {
+		int numPackets = (int) Math.ceil((double) fileData.length / MSS);		
+		String data = new String(fileData);
+		for (int segNum = 0; (segNum < numPackets) && ((segNum * MSS) < data.length()); segNum++) {
+			int segEnd = Math.min((MSS * (segNum + 1)), data.length());
+			String segData = data.substring(MSS * segNum, segEnd);
+			segment s = new segment(segNum, segData);
+			if (head == null) {
+				head = s;
+			} 
+			else {
+				segment temp = head;
+				while (temp.next != null) 
+					temp = temp.next;
+				temp.next = s;
+			}
+		}
+		return numPackets;
+	}
+
+	public static int getACKNum(byte[] data) {
+		String ACK = "";
+		for (int i = 0; i < 64; i++) {
+			if (data[i] == 48) {
+				ACK += "0";
+			} else {
+				ACK += "1";
+			}
+		}
+		String dataType = ACK.substring(48, 64);
+		if (dataType.equals("1010101010101010")) {
+			return decimal(ACK.substring(0, 32));
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) //throws IOException
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the server ip address:");
+		String host = s.next();
+		System.out.println("Enter the port number of server:");
+		int port = s.nextInt();
+		System.out.println("Enter the path of the file to be transfered:");
+		String fileName  = s.next();
+		System.out.println("Enter the window size:");
+		int N = s.nextInt();
+		System.out.println("Enter the maximum segment size:");
+		int MSS = s.nextInt();
+		s.close();
+		DatagramSocket clientSocket = null;
+		InetAddress serverIP = null;
+		byte[] fileData = null;
+		try {
+			fileData = Files.readAllBytes(new File(fileName).toPath());
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
+		try{
+			clientSocket = new DatagramSocket(); //port
+			}
+		catch (SocketException e1){
+			e1.printStackTrace();
+		}
+
+		try{
+			serverIP = InetAddress.getByName(host);
+		}
+		catch (UnknownHostException e1){
+			e1.printStackTrace();
+		}
+		
+		int nPackets = generateSegments(fileData, MSS);
+
+		int curSeg = 0;
+		int segPointer = 0;
+		long tic = System.currentTimeMillis();
+		int ackSeqNum = -1;
+		
+		while(curSeg < nPackets){
+			while(segPointer < N && (curSeg < nPackets)){
+				segment temp = head;
+				while((temp.next!=null) && (temp.id != curSeg))
+					temp = temp.next;
+				String segData = temp.data;
+				byte[] segBytes = null;
+				segBytes = segData.getBytes();
+				byte[] segHeader = generateHeader(curSeg, segBytes);
+
+				byte[] packet = new byte[segHeader.length + segBytes.length];
+				System.arraycopy(segHeader, 0, packet, 0, segHeader.length);
+				System.arraycopy(segBytes, 0, packet, segHeader.length, segBytes.length);
+
+				DatagramPacket toReceiver = new DatagramPacket(packet, packet.length, serverIP, port);
+				
+				try {
+					clientSocket.send(toReceiver);
+					System.out.println("Packet sent:" + curSeg);
+					curSeg++;
+					segPointer++;
+				} 
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+
+			int timeout = 1000;// in milliseconds
+			byte[] receive = new byte[1536]; //WHY??????
+			DatagramPacket fromReceiver = new DatagramPacket(receive, receive.length);
+	
+			int temp = curSeg;
+			
+			try {
+				clientSocket.setSoTimeout(timeout);
+				while (true) {
+					clientSocket.receive(fromReceiver);
+					ackSeqNum = getACKNum(fromReceiver.getData());
+					System.out.println("ACK received: " + ackSeqNum);
+					if (ackSeqNum == temp - 1) {
+						segPointer = 0;
+						curSeg = temp;
+						break;
+					} 
+					else if (ackSeqNum != -1) { // any other acknowledgement
+						segPointer = curSeg - ackSeqNum - 1;
+						curSeg = ackSeqNum + 1;
+					}
+				}
+			} 
+			catch (IOException ste) {// timeout
+				System.out.println("Timeout, sequence number = " + ackSeqNum);
+				curSeg = ackSeqNum + 1;
+				segPointer = 0;
+			} 
+			
+		}
+		//Send EOF Packet
+		String EOF = "000000000000000000000000000000000000000000000000000000000000000000000000000";
+		byte[] EOFbytes = EOF.getBytes();
+		DatagramPacket eofPacket = new DatagramPacket(EOFbytes, EOFbytes.length, serverIP, port);
+		
+		try {
+			clientSocket.send(eofPacket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		long toc = System.currentTimeMillis();
+		System.out.println("Total transfer time is " + (toc - tic) + " ms");
+	}
+}
---
+++ src/server.java
@@ -0,0 +1,138 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class server{
+
+	private static int decimal(String substring){
+		//Convert string to decimal integer value
+		int decimalValue = Integer.parseInt(substring, 2);
+		return decimalValue;
+	}
+
+	public static int calcChecksum(byte[] buf) {
+		//Return a 16-bit checksum for a byte array payload (decimal)
+		int length = buf.length;
+	    int i = 0;
+	    int sum = 0;
+	    while (length > 0) {
+	        sum += (buf[i++]&0xff) << 8;
+	        if ((--length)==0) break;
+	        sum += (buf[i++]&0xff);
+	        --length;
+	    }
+	    int checksum =  (~((sum & 0xFFFF)+(sum >> 16)))&0xFFFF;
+	    return decimal(String.format("%16s", Integer.toBinaryString(checksum)).replace(' ', '0'));
+	}
+
+	public static boolean matchChecksum(int newChecksum, int oldChecksum){
+		//Return True if two checksums are equal, else False
+		newChecksum = Integer.parseInt("FFFF", 16) - newChecksum;
+		long test = newChecksum + oldChecksum;
+		test =  Integer.parseInt("FFFF", 16) - test;
+		return (test == 0);
+	}
+
+	public static byte[] sendAck(int num){
+		//Creates ACK Packet
+		String header = Integer.toBinaryString(num);
+		for(int i = header.length(); i < 32; i++)
+			header = "0" + header;
+		header = header + "00000000000000001010101010101010";
+		return header.getBytes();
+	}
+
+	public static void main(String[] args){
+		//Enter user inputs
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the server port number:");
+		int port = s.nextInt();
+		System.out.println("Enter the path of the output file:");
+		String outputFile = s.next();
+		System.out.println("Enter the probability of loss:");
+		double probLoss =  s.nextDouble();
+		s.close();
+		
+		//Create server socket
+		DatagramSocket serverSocket = null;		
+		try{
+			serverSocket = new DatagramSocket(port);
+			}
+		catch (SocketException e1){
+			System.out.println("I am here!!");
+			e1.printStackTrace();
+		}
+
+		int count = 0;
+		ByteArrayOutputStream msgBuffer = new ByteArrayOutputStream();
+		DatagramPacket fromClient = null;
+
+		while (true){
+			try{
+				byte[] dataPacket = new byte[2048];
+				//Receive data packet from Client
+				fromClient = new DatagramPacket(dataPacket, dataPacket.length);
+				serverSocket.receive(fromClient);
+		
+				String data = new String(fromClient.getData());
+				
+				//Extract sequence num, checksum, packetType and message from received packet
+				int seqNum = decimal(data.substring(0,32));
+				System.out.println("Received packet " + seqNum);
+				int oldChecksum = decimal(data.substring(32,48));
+				String dataType = data.substring(48,64);
+				String message = data.substring(64, data.length());
+				
+				//calculate new checksum on message and compare
+				int newChecksum = calcChecksum(message.getBytes());
+				boolean matchCS = matchChecksum(newChecksum, oldChecksum);
+
+				double randLoss = Math.random();
+
+				if (dataType.equals("0000000000000000")) 
+					//EOF Packet
+						break;
+
+				if (randLoss <= probLoss){
+					System.out.println("Packet loss, sequence number = " + seqNum);
+					continue;
+				}
+
+				else if (matchCS && seqNum == count){
+					//Packet received correctly
+					msgBuffer.write(message.getBytes());
+					int portNum = fromClient.getPort();
+					InetAddress IPadd = fromClient.getAddress();
+					byte[] ackPacket = sendAck(seqNum);
+					DatagramPacket toClient = new DatagramPacket(ackPacket, ackPacket.length, IPadd, portNum);
+					serverSocket.send(toClient);
+					System.out.println("Sent ACK " + seqNum);
+					count++;
+				}
+
+			}
+			catch (Exception e) {
+				System.err.println(e);
+		}
+
+		}
+		serverSocket.close();
+		
+		//Create file and flush buffer data to file
+		FileOutputStream file = null;
+		try {
+			file = new FileOutputStream(outputFile);
+			msgBuffer.writeTo(file);
+			file.close();
+			System.out.println("File created!");
+		} catch( Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
